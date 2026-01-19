1a0be7af06cb669a088a83aea4faa86beba0a19c
cryptobull <musalawal04@gmail.com>
2026-01-19 22:58:57 +0100

    uifixed

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -81,9 +81,9 @@ const questions = [
 ];
 
 const FAQ = () => {
-  const [activeIndex, setActiveIndex] = useState(null);
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
-  const toggleFAQ = (index) => {
+  const toggleFAQ = (index: number) => {
     setActiveIndex(activeIndex === index ? null : index);
   };
 
